c7a88cb425e6c14003c47abb19771995ba270a49
Husain Shahid Rao <hsr3012kalimg@gmail.com>
2021-06-30 02:22:39 +0530

    add delete

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const findOrCreate = require("mongoose-findorcreate");
 const e = require("express");
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
@@ -23,12 +23,12 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
-mongoose.connect("mongodb+srv://admin-husain:"+process.env.ATLASPW+"@cluster0.ouw0w.mongodb.net/SayItOut?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: true });
+mongoose.connect("mongodb+srv://admin-husain:" + process.env.ATLASPW + "@cluster0.ouw0w.mongodb.net/SayItOut?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: true });
 
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true },
   password: String,
-  confessions: Array
+  confessions: Array,
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -46,30 +46,26 @@ passport.deserializeUser(function (id, done) {
     done(err, user);
   });
 });
-app.get("/", (req, res)=> {
-    res.redirect("/home")
-})
+app.get("/", (req, res) => {
+  res.redirect("/home");
+});
 app.get("/home", (req, res) => {
   console.log(req.isAuthenticated());
   if (req.isAuthenticated()) {
     //   console.log(req.user)
-      User.findById(req.user.id, function (err, user) {
-          if(err){
-              console.log(err)
-          }
-          else{
-              console.log(user.confessions.length)
-             return user;
-          }
-      })
-    
+    User.findById(req.user.id, function (err, user) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(user.confessions.length);
+        return user;
+      }
+    })
     .then((user) => {
-        console.log(user)
-       
-        res.render("home", { confessions: user.confessions, username: user.username });
+      console.log(user);
 
-
-    });  
+      res.render("home", { confessions: user.confessions, username: user.username });
+    });
   } else {
     res.redirect("login");
   }
@@ -92,11 +88,10 @@ app.post("/register", (req, res) => {
 });
 
 app.get("/login", (req, res) => {
-  if(req.isAuthenticated()) {
+  if (req.isAuthenticated()) {
     res.redirect("/");
-  }else{
+  } else {
     res.render("login", { failed: false });
-
   }
 });
 
@@ -111,47 +106,73 @@ app.get("/register-failed", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  
-  
-    const user = new User({
-      username: req.body.username,
-      password: req.body.password,
-    });
-    req.login(user, (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        passport.authenticate("local", { failureRedirect: "/login-failed" })(req, res, function () {
-          res.redirect("/home");
-        });
-      }
-    });
-
-  
- 
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+  req.login(user, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local", { failureRedirect: "/login-failed" })(req, res, function () {
+        res.redirect("/home");
+      });
+    }
+  });
 });
 
 app.post("/confess", (req, res) => {
   let toUser = req.body.toUser;
   confessionMsg = req.body.confession;
   User.findOne({ username: toUser }, (foundUser) => {
-      if(foundUser!=null){
-        return foundUser;
-
-      }
-  }).then((foundUser) => {
-    console.log(foundUser);
-    confession = {
-        msg: confessionMsg,
-        date: Date.now()
+    if (foundUser != null) {
+      return foundUser;
     }
+  }).then((foundUser) => {
+    confession = {
+      msg: confessionMsg,
+      date: Date.now(),
+     
+    };
     foundUser.confessions.push(confession);
-    foundUser.save(err=>{
-        if(!err) {
-            res.redirect("home")
-        }
+    foundUser.save((err) => {
+      if (!err) {
+        res.redirect("home");
+      }
     });
   });
+});
+
+app.post("/post/delete", (req, res) => {
+
+  let username = req.user.username;
+  console.log(username + " is triyntto feete")
+  let id = req.body.confessionId;
+  let newConfessions = [];
+  User.findOne({ username: username }, (foundUser) => {
+    if(foundUser){
+      return foundUser;
+    }else{
+      console.log(foundUser)
+    }
+   
+  })
+  .then((foundUser) => {
+    let len = foundUser.confessions.length;
+    for (i = 0; i < len; i++) {
+      if (foundUser.confessions[i].date!= id) {
+        newConfessions.push(foundUser.confessions[i]);
+      }
+    }
+    foundUser.confessions = newConfessions;
+    foundUser.save(err=>{
+      if(!err) {
+        res.redirect("/home")
+      }else{
+        res.send(err);
+      }
+    })
+  })
 });
 
 app.get("/logout", function (req, res) {
@@ -161,16 +182,13 @@ app.get("/logout", function (req, res) {
 
 app.get("/post/:username", (req, res) => {
   let username = req.params.username;
-  User.findOne({username: username}, (err, user) => {
-      if(!err && user!=null){
-        res.render("confess", { toUser: username });
-
-      }
-      else{
-          res.render("404")
-      }
-  })
-
+  User.findOne({ username: username }, (err, user) => {
+    if (!err && user != null) {
+      res.render("confess", { toUser: username });
+    } else {
+      res.render("404");
+    }
+  });
 });
 app.listen(process.env.PORT || 3000, function () {
   console.log("Server started on port 3000");
