e83d68682a8d3f9766d5057441a1a5f0e8d92451
Husain Shahid Rao <hsr3012kalimg@gmail.com>
2021-06-29 18:23:27 +0530

    debugging

--- public/js/main.js
+++ public/js/main.js
@@ -2,4 +2,4 @@ let originUrl = document.location.origin;
 
 let username = $("#post-url-self").text();
 $("#post-url-self").text(originUrl + "/post/" + username);
-$("#post-url-self").attr("href", originUrl + "/post/" + username);
+$("#post-url-self").attr("href", $("#post-url-self").text());
