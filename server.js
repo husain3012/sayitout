8471ac96eaf70caf02da1a54ef437c1cfe290a7a
Devansh Singh Parmar <devansh18dp@gmail.com>
2021-07-01 09:19:24 +0530

    UI

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
-mongoose.connect("mongodb+srv://admin-husain:" + process.env.ATLASPW + "@cluster0.ouw0w.mongodb.net/SayItOut?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: true });
+mongoose.connect("mongodb://localhost:27017/sayitout", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: true });
 
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true },
